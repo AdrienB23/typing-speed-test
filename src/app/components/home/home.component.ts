bb1ccaa2d5fe9ee48687f4cb689f245a94e6fc6f
Adrien Baffioni <baffioniadrien23@gmail.com>
2026-01-06 17:57:16 +0100

    feat(home): add stats display and settings controls

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SelectButton } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    SelectButton,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  @Input() wpm: number = 0;
+  @Input() accuracy: number = 100;
+  @Input() time: number = 60;
+  difficultyOptions = [{ label: "Easy", value: "easy" }, { label: "Medium", value: "medium" }, { label: "Hard", value: "hard" }]
+  modeOptions = [{ label: "Timed(60s)", value: "time" }, { label: "Passage", value: "passage" }]
+
+  selectedDifficulty: string = "easy";
+  selectedMode: string = "time";
 
 }
